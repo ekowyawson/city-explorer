f6221ce1248d4d8654c93e3a318afee69c4ff9a5
Ekow Yawson <ekowyawson71@gmail.com>
2023-12-07 05:22:47 -0500

    Completed lab8: Add dynamic weather.

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -5,82 +5,44 @@ import Col from 'react-bootstrap/Col';
 
 import styles from './weather.module.css';
 
-// const cl = i => console.log(i);
-
 function Weather(props) {
-
+    // let city = props.city;
     let weatherData = props.weatherData;
-    let city = props.city;
 
     return (
         <section className={styles.layout}>
-            <h6 className={styles.heading}>Weather Data</h6>
-            
+            <h6 className={styles.heading}>16 Day Weather Forecasts</h6>
+
             <div className='alldata'>
-                <Container className='alldataContainer'>
-                    {/* TIMEZONE */}
-                    <Row>
-                        <Col xs={4} md={4}>
-                            <p className='datakey'>Time Zone:</p>
-                        </Col>
-                        <Col xs={4} md={4}>
-                            <p className='dataval'>{weatherData.timezone}</p>
-                        </Col>
-                        <Col xs={4} md={4}>
-                            <p className='dataval'>{weatherData.timezone}</p>
-                        </Col>
-                    </Row>
+                    <Container className='alldataContainer'>
+                        <Row>
+                            <Col xs={4} md={4}>
+                                <p className='datakey'>Date:</p>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <p className='datakey'>Temperature</p>
+                            </Col>
+                            <Col xs={4} md={4}>
+                                <p className='datakey'>Weather</p>
+                            </Col>
+                        </Row>
 
-                    {/* COUNTRY CODE */}
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <p className='datakey'>Country Code:</p>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <p className='dataval'>{weatherData.country_code}</p>
-                        </Col>
-                    </Row>
-
-                    {/* DATE */}
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <p className='datakey'>Date:</p>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <p className='dataval'>{weatherData.data[0].datetime}</p>
-                        </Col>
-                    </Row>
-
-                    {/* MAX TEMP */}
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <p className='datakey'>Max Temperature:</p>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <p className='dataval'>{weatherData.data[0].high_temp} &deg;C</p>
-                        </Col>
-                    </Row>
-
-                    {/* MIN TEMP */}
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <p className='datakey'>Min Temperature:</p>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <p className='dataval'>{weatherData.data[0].low_temp} &deg;C</p>
-                        </Col>
-                    </Row>
-
-                    {/* WEATHER DESCRIPTION */}
-                    <Row>
-                        <Col xs={6} md={4}>
-                            <p className='datakey'>Weather:</p>
-                        </Col>
-                        <Col xs={6} md={4}>
-                            <p className='dataval'>{weatherData.data[0].weather.description}</p>
-                        </Col>
-                    </Row>
-                </Container>
+                        {weatherData.map((data, key) => {
+                            return (
+                                <Row key={key}>
+                                    <Col xs={4} md={4}>
+                                        <p className='dataval'>{data.date}</p>
+                                    </Col>
+                                    <Col xs={4} md={4}>
+                                        <p className='dataval'>{data.temp}&deg;C</p>
+                                    </Col>
+                                    <Col xs={4} md={4}>
+                                        <p className='dataval'>{data.weather}</p>
+                                    </Col>
+                                </Row>
+                            )
+                        })}
+                    </Container>
             </div>
         </section>
     )
